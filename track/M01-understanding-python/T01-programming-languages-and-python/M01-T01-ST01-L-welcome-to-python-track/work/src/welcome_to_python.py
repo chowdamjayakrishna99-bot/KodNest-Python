22a81595ba40ee75c24e1cc9a612e04fe694a9e8
Jaya Krishna <chowdamjayakrishna99@gmail.com>
2026-08-03 17:15:39 +0530

    day1krsh

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/welcome_to_python.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/welcome_to_python.py
@@ -1,1 +1,8 @@
-print("i am krisihan")
+student_name="krishna"
+course_duration=30
+course_rating=4.8
+is_enrolled=True
+print(type(student_name))
+print(type(course_duration))
+print(type(course_rating))
+print(type(is_enrolled))
