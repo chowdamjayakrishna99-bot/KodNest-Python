631092239771ce523dfd44032ca323980d02b394
Jaya Krishna <chowdamjayakrishna99@gmail.com>
2026-08-04 18:28:32 +0530

    part1

---
+++ track/mo2/t03/src/calculator.py
@@ -0,0 +1,14 @@
+from ast import operator
+num1=int(input("enter the num1:"))
+num2=int(input("enter the num2:"))
+operator=input("enter the operator:")
+if(operator=="+"):
+    print(num1+num2)
+elif(operator=="-"):
+    print(num1-num2)
+elif(operator=="*"):
+    print(num1*num2)
+elif(operator=="/"):
+    print(num1/num2)
+else:
+    print("invalid operator")
