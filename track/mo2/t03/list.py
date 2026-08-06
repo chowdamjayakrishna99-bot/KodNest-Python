df7db7a63dbfa8510207390483f75286448de22b
Jaya Krishna <chowdamjayakrishna99@gmail.com>
2026-08-06 17:02:42 +0530

    krishn1

---
+++ track/mo2/t03/list.py
@@ -0,0 +1,13 @@
+lis=[10,[20,30],(40,50),{60,70,80},{1:'a',2:'b'}]
+
+print(lis)
+print((lis[0]))
+print((lis[1]))
+print((lis[2]))
+print((lis[3]))
+print((lis[4]))
+print((lis[-1]))
+print((lis[-2]))
+print((lis[-3]))
+print((lis[-4]))
+print((lis[-5]))
--- track/mo2/t03/src/for.py
+++ track/mo2/t03/src/for.py
@@ -1,2 +1,2 @@
-for i in range(11,0,-1):
+for i in range(10,0,-1):
     print(i)
